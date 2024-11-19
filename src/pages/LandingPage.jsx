a872d988717c4dc84100da250acdd33c96a8c034
Ndumiwam <noceemasondo28@gmail.com>
2024-11-19 08:44:11 +0200

    initial

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div className="w-(100vw) flex h-(100vh)">
+      {/*blue background div*/}
+      <div className="w-(50vw)"></div>
+      {/*white background div*/}
+      <div></div>
+    </div>
+  );
+}
+
+export default LandingPage;
