0add425d50c0151d3570f32322eeff6fcf11626c
Ndumiwam <noceemasondo28@gmail.com>
2024-11-19 09:04:34 +0200

    still

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,7 +4,7 @@ function LandingPage() {
   return (
     <div className="w-(100vw) flex h-(100vh)">
       {/*blue background div*/}
-      <div className="w-(50vw)"></div>
+      <div className="w-(50vw)"><img src="/image/screenshot 2024-11-19" alt="background" /></div>
       {/*white background div*/}
       <div></div>
     </div>
